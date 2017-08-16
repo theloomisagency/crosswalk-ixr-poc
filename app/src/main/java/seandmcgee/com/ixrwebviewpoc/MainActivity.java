1d87fbdc841758866a0d87b193d5d4d1f136a20a
smcgee <sean.mcgee@kannuu.com>
2017-08-16 11:43:41 -0500

    Full KitKat Compat + Modal Webview

--- app/src/main/java/seandmcgee/com/ixrwebviewpoc/MainActivity.java
+++ app/src/main/java/seandmcgee/com/ixrwebviewpoc/MainActivity.java
@@ -1,13 +1,23 @@
 package seandmcgee.com.ixrwebviewpoc;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 
 import org.xwalk.core.XWalkPreferences;
 import org.xwalk.core.XWalkView;
+import android.view.View;
+import android.webkit.WebView;
+import android.app.Dialog;
+import android.widget.TextView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
     private XWalkView xWalkWebView;
+    WebView web;
+    Button adLink;
+    Dialog web_dialog;
+    TextView AdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +25,35 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         xWalkWebView=(XWalkView)findViewById(R.id.xwalkWebView);
-        xWalkWebView.load("http://cdpn.io/NPwjGE", null);
+        xWalkWebView.load("https://codepen.io/chrisgannon/full/NPwjGE", null);
 
         // turn on debugging
         XWalkPreferences.setValue(XWalkPreferences.REMOTE_DEBUGGING, true);
+        AdView =(TextView)findViewById(R.id.AdView);
+        adLink = (Button)findViewById(R.id.defaultAd);
+        adLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                web_dialog = new Dialog(MainActivity.this);
+                web_dialog.setContentView(R.layout.web_layout);
+                web = (WebView) web_dialog.findViewById(R.id.webview);
+                web.getSettings().setJavaScriptEnabled(true);
+
+                web.loadUrl("http://www.google.com");
+
+                web.setWebViewClient(new WebViewClient() {
+                    @Override
+                    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                        view.loadUrl(url);
+                        return true;
+                    }
+
+                });
+                web_dialog.show();
+                web_dialog.setTitle("Some Advertisement");
+                web_dialog.setCancelable(true);
+            }
+        });
     }
 
     @Override
